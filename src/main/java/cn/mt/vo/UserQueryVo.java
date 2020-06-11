474965faef50ae8ba611646e64f4bfada0b6248d
loumt <lmtemail163@163.com>
2020-06-11 11:32:40 +0800

    spring 未完成

--- src/main/java/cn/mt/vo/UserQueryVo.java
+++ src/main/java/cn/mt/vo/UserQueryVo.java
@@ -1,6 +1,7 @@
 package cn.mt.vo;
 
 import cn.mt.models.User;
+import lombok.ToString;
 
 import java.util.List;
 
@@ -11,6 +12,7 @@ import java.util.List;
  * @Description TODO
  * @Date 2020/6/9 13:38
  */
+@ToString
 public class UserQueryVo {
     private User user;
 
